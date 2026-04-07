3bdf7db9e83dc208ee958414f8bab5c26562a061
Daniek Schuiling <daniekschuiling@gmail.com>
2026-04-07 19:47:40 +0200

    Search added

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,17 +1,30 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function(Controller, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function(Controller, JSONModel, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("product.dashboard.controller.App", {
         onInit: function() {
             var sUrl = "https://services.odata.org/V4/Northwind/Northwind.svc/Products?$top=20&$format=json";
-
             var oModel = new JSONModel();
             oModel.loadData(sUrl);
-
             this.getView().setModel(oModel);
+        },
+
+        onSearch: function(oEvent) {
+            var sQuery = oEvent.getParameter("newValue");
+            var oTable = this.byId("productTable");
+            var oBinding = oTable.getBinding("items");
+
+            if (sQuery) {
+                var oFilter = new Filter("ProductName", FilterOperator.Contains, sQuery);
+                oBinding.filter([oFilter]);
+            } else {
+                oBinding.filter([]);
+            }
         }
     });
 });
